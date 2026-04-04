23b977492a1cbb64cae2391c685bb53b98597b09
Sienna <sgoldenbird@gmail.com>
2026-04-04 18:36:29 +0900

    refactor: remove scripts and workflows, add trigger workflows for automation repo

--- .github/scripts/utils/date.js
+++
@@ -1,6 +0,0 @@
-export const getKSTDateString = (date) => {
-  const kstOffset = 9 * 60 * 60 * 1000;
-  const kstDate = new Date(date.getTime() + kstOffset);
-
-  return kstDate.toISOString().split("T")[0];
-};
