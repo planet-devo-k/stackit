c5d27f2af68792c7697cd8ff8d38148a3013d0bb
Sienna <sgoldenbird@gmail.com>
2026-06-11 21:52:21 +0900

    study: week7 정리

---
+++ 알고리즘_도감/sgoldenbird/week7.js
@@ -0,0 +1,14 @@
+// 1. 하이브리드 암호화 메시지 생성
+function solution1() {}
+
+// 2. 디피-헬만 키 교환 과정 시뮬레이션
+function solution2() {}
+
+// 3. 메시지 인증 코드 (MAC) 생성 및 검증
+function solution3() {}
+
+// 4. 전자 서명 검증
+function solution4() {}
+
+// 5. 전자 인증서 정보 추출
+function solution5() {}
